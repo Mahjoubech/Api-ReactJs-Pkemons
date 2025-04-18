87786b4c45af34833f3d77ec525ccf47e39f671b
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-04-18 17:23:04 +0100

    remove naav from bck compnnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Navbar from './components/Navbar';
 import PokemonList from './components/PokemonList';
 import PokemonDetail from './components/PokemonDetail';
 
@@ -23,7 +22,7 @@ function App() {
         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
           <div className="absolute inset-0 bg-[url('/grid.svg')] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
           <div className="relative z-10">
-            <Navbar />
+           
             <Routes>
               <Route path="/" element={<PokemonList />} />
               <Route path="/pokemon/:id" element={<PokemonDetail />} />
