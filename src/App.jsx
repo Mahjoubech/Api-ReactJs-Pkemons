b289e0394b285ccc601311c5b4105e29327d698d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-04-18 12:11:38 +0100

    Update App.jsx to include React Query Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PokemonList from './components/PokemonList';
 import PokemonDetail from './components/PokemonDetail';
 import Navbar from './components/Navbar';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 300000, // 5 minutes
+      cacheTime: 900000, // 15 minutes
+      retry: 2,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<PokemonList />} />
-          <Route path="/pokemon/:id" element={<PokemonDetail />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<PokemonList />} />
+            <Route path="/pokemon/:id" element={<PokemonDetail />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
